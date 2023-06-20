8c77e7793e0ad920c65c781a63a560aa4972de6f
tuanhung96 <tuanhung.hn96@gmail.com>
2023-06-21 00:11:03 +0700

    update home page

--- src/main/java/com/example/asm2/controller/PublicController.java
+++ src/main/java/com/example/asm2/controller/PublicController.java
@@ -40,45 +40,45 @@ public class PublicController {
     @RequestMapping ("/home")
     public String home(Model theModel, Principal principal) {
 
-//        List<String[]> companies = getInformationOfTop5Company();
-//        theModel.addAttribute("companies", companies);
-//
-//        List<Recruitment> recruitments = getTop5Recruitment();
-//        theModel.addAttribute("recruitments", recruitments);
+        List<String[]> companies = getInformationOfTop5Company();
+        theModel.addAttribute("companies", companies);
+
+        List<Recruitment> recruitments = getTop5Recruitment();
+        theModel.addAttribute("recruitments", recruitments);
+
+        if (principal != null) {
+            User user = userService.findByEmail(principal.getName());
+            theModel.addAttribute("user", user);
+        }
 
 //        List<Category> categories = categoryService.getTop4Category();
 //        theModel.addAttribute("categories", categories);
 
-//        if (principal != null) {
-//            User user = userService.findByEmail(principal.getName());
-//            theModel.addAttribute("user", user);
-//        }
-
         return "public/home";
     }
 
-//    public List<String[]> getInformationOfTop5Company() {
-//        List<Integer> top5CompanyId = recruitmentService.getTop5CompanyId();
-//        List<String[]> companies = new ArrayList<>();
-//        for (int i=0; i<top5CompanyId.size();i++) {
-//            Company company = companyService.findById(top5CompanyId.get(i));
-//            String[] companyInformationNeeded = new String[4];
-//            companyInformationNeeded[0] = String.valueOf(company.getId());
-//            companyInformationNeeded[1] = company.getNameCompany();
-//            companyInformationNeeded[2] = company.getLogo();
-//            companyInformationNeeded[3] = String.valueOf(company.getRecruitments().size());
-//            companies.add(companyInformationNeeded);
-//        }
-//        return companies;
-//    }
-//
-//    private List<Recruitment> getTop5Recruitment() {
-//        List<Integer> top5RecruitmentId = applypostService.getTop5RecruitmentId();
-//        List<Recruitment> recruitments = new ArrayList<>();
-//        for (int i=0; i<top5RecruitmentId.size();i++) {
-//            Recruitment recruitment = recruitmentService.findById(top5RecruitmentId.get(i));
-//            recruitments.add(recruitment);
-//        }
-//        return recruitments;
-//    }
+    public List<String[]> getInformationOfTop5Company() {
+        List<Integer> top5CompanyId = recruitmentService.getTop5CompanyId();
+        List<String[]> companies = new ArrayList<>();
+        for (int i=0; i<top5CompanyId.size();i++) {
+            Company company = companyService.findById(top5CompanyId.get(i));
+            String[] companyInformationNeeded = new String[4];
+            companyInformationNeeded[0] = String.valueOf(company.getId());
+            companyInformationNeeded[1] = company.getNameCompany();
+            companyInformationNeeded[2] = company.getLogo();
+            companyInformationNeeded[3] = String.valueOf(company.getRecruitments().size());
+            companies.add(companyInformationNeeded);
+        }
+        return companies;
+    }
+
+    public List<Recruitment> getTop5Recruitment() {
+        List<Integer> top5RecruitmentId = applypostService.getTop5RecruitmentId();
+        List<Recruitment> recruitments = new ArrayList<>();
+        for (int i=0; i<top5RecruitmentId.size();i++) {
+            Recruitment recruitment = recruitmentService.findById(top5RecruitmentId.get(i));
+            recruitments.add(recruitment);
+        }
+        return recruitments;
+    }
 }
