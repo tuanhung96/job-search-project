fae5dceace3d10a99f184f92753b9f16cd2410ae
tuanhung96 <tuanhung.hn96@gmail.com>
2023-06-21 15:51:16 +0700

    update

--- src/main/java/com/example/asm2/config/ResourceWebConfig.java
+++ src/main/java/com/example/asm2/config/ResourceWebConfig.java
@@ -1,8 +1,6 @@
 package com.example.asm2.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
--- src/main/java/com/example/asm2/config/SecurityConfig.java
+++ src/main/java/com/example/asm2/config/SecurityConfig.java
@@ -44,6 +44,7 @@ public class SecurityConfig {
                         .requestMatchers("/home").permitAll()
                         .requestMatchers("/register").permitAll()
                         .requestMatchers("/verify").permitAll()
+                        .requestMatchers("/uploads/**").permitAll()
                         .requestMatchers("/candidate/**").hasRole("CANDIDATE")
                         .requestMatchers("/employer/**").hasRole("EMPLOYER")
                         .anyRequest().authenticated()
